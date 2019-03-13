ec2b7f71520b29a5918dd97291ea6e00ba1e1c78
Markus Pettersson <markus.pettersson1998@gmail.com>
2019-03-13 15:22:41 +0100

    Simplified code.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,12 +17,10 @@ public class Main {
             .version(HttpClient.Version.HTTP_1_1)
             .build();
 
-
     public static void main(String[] args) throws IOException {
         // 1. Read password from file
         // 2. Hash passwords with SHA-1
         // 3. Take only the first 5 hexadecimal characters of each password
-        final int passwordCharacterLimit = 5;
         // 4. For every password, make a request to a certain API
         // 5. After a response, compare every password with response from API
         // 6. If there's a match, print how many times the password has leaked
@@ -35,45 +33,41 @@ public class Main {
                 .map(String::toUpperCase)
                 .collect(Collectors.toList());
 
-        var hashedShortenedPasswords = hashedPasswords.stream()
-                // 3
-                .map(password -> password.substring(0, passwordCharacterLimit))
-                .collect(Collectors.toList());
-
-        var pwnedPasswords = hashedShortenedPasswords.stream()
+        var pwnedPasswords = hashedPasswords.stream()
                 // 4
-                .map(Main::haveIBeenPwned)
-                .flatMap(Optional::stream)
-                .flatMap(List::stream)
-                .flatMap(pwned -> hashedShortenedPasswords.stream()
-                        .map(hashed -> hashed.concat(pwned))
-                )
-                .collect(Collectors.toList());
-
+                .collect(Collectors.toMap(Object::toString,
+                                            Main::haveIBeenPwned));
 
         // Have I been pwned?
-        pwnedPasswords.stream()
+        hashedPasswords.stream()
+                .map(pwnedPasswords::get)
+                .flatMap(Optional::stream)
+                .flatMap(List::stream)
                 // 5.
                 .filter(pwnedPassword -> hashedPasswords.contains(pwnedPassword.substring(0, pwnedPassword.indexOf(":"))))
                 // 6.
                 .forEach(System.out::println);
-
-
     }
 
     public static Optional<List<String>> haveIBeenPwned(String passwordHash) {
+        // We only need to send the first 5 characters of the hashed password to HaveIBeenPwned's API
         final String baseURL = "https://api.pwnedpasswords.com/range/";
-        var apiURL = baseURL.concat(passwordHash);
-        // Make request to API with given string/password
+        final int PASSWORDCHARACTERLIMIT = 5;
+        // 3
+        var shortPasswordHash = passwordHash.substring(0, PASSWORDCHARACTERLIMIT);
+        var apiURL = baseURL.concat(shortPasswordHash);
 
+        // Make request to API
         HttpRequest request = HttpRequest.newBuilder()
                                 .uri(URI.create(apiURL))
                                 .build();
+
         try {
             var respone = httpClient.send(request, HttpResponse.BodyHandlers.ofLines());
-
-            return Optional.of(respone.body()
-                    .collect(Collectors.toList()));
+            return Optional.of(
+                respone.body()
+                        .map(shortPasswordHash::concat)
+                        .collect(Collectors.toList()));
 
         } catch (Exception e) {
             return Optional.empty();
